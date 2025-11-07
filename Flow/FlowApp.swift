e5b49310987908ee47fd0c7cadf4c3be9520494a
almasrauder <darking.picqaxe@gmail.com>
2025-11-07 23:50:56 +0500

    Initial Commit

---
+++ Flow/FlowApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlowApp.swift
+//  Flow
+//
+//  Created by Darking Almas on 07.11.2025.
+//
+
+import SwiftUI
+
+@main
+struct FlowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
